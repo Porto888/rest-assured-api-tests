e9ca89c797f6952f9e0b8d44634754497a3eb6b2
Porto888 <guilhermepstk@hotmail.com>
2024-01-01 18:55:59 -0300

    Inclução de um "@BeforeAll", visando melhorar a legibilidade e também a escalabilidade do projeto.

--- src/main/java/XmlValidations.java
+++ src/main/java/XmlValidations.java
@@ -1,6 +1,12 @@
-import groovyjarjarantlr4.v4.runtime.tree.TerminalNodeImpl;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.path.xml.element.Node;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -10,15 +16,35 @@ import static org.hamcrest.Matchers.*;
 
 public class XmlValidations {
 
+    public static RequestSpecification reqSpec;
+    public static ResponseSpecification respSpec;
+
     //    Abaixo temos exemplos básicos de como realizar  validações em um arquivo ".XML"
+@BeforeClass
+    public static void setUp(){
+        RestAssured.baseURI = "http://restapi.wcaquino.me";
+
+    RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+    reqBuilder.log(LogDetail.ALL);
+    reqSpec = reqBuilder.build();
+
+    ResponseSpecBuilder respBuilder = new ResponseSpecBuilder();
+    respBuilder.expectStatusCode(200);
+    respSpec = respBuilder.build();
+
+    RestAssured.requestSpecification = reqSpec;
+    RestAssured.responseSpecification = respSpec;
+    }
     @Test
     public void validandoObjetoNumArquivoXML() {
-        given()
-                .when()
-                .get("http://restapi.wcaquino.me/usersXML/3")
-                .then()
-                .statusCode(200)
 
+        given()
+                .spec(reqSpec)
+                .when()
+                .get("usersXML/3")
+                .then()
+//                .statusCode(200)
+                .spec(respSpec)
                 .rootPath("user")
                 .body("name", is("Ana Julia"))
                 .body("@id", is("3"))
@@ -43,10 +69,11 @@ public class XmlValidations {
     @Test
     public void validarArqXML() {
         given()
+                .spec(reqSpec)
                 .when()
-                .get("http://restapi.wcaquino.me/usersXML")
+                .get("usersXML")
                 .then()
-                .statusCode(200)
+                .spec(respSpec)
                 .body("users.user.size()", is(3))
                 .body("users.user.findAll{it.age.toInteger() <= 25}.size()", is(2))
                 .body("users.user.@id", hasItems("1", "2", "3"))
@@ -63,33 +90,36 @@ public class XmlValidations {
     //    Aqui estão exemplificadas algumas verificações e asserções um pouco mais avançadas, utilizando 'XMLpath + Java"
 
     @Test
-    public void realizandoTestesMaisAvancadoXmlPathInJava (){
-//        Object path = given()
-//                .when()
-//                .get("http://restapi.wcaquino.me/usersXML")
-//                .then()
-//                .statusCode(200)
-//                .extract().path("users.user.name.findAll{it.toString().startsWith('Maria')}")
-//
-//                ;
-//        System.out.println(path.toString());
+    public void realizandoTestesMaisAvancadosComXmlPath(){
+        Object path = given()
+                .spec(reqSpec)
+                .when()
+                .get("usersXML")
+                .then()
+                .spec(respSpec)
+                .extract().path("users.user.name.findAll{it.toString().startsWith('Maria')}")
+
+                ;
+        System.out.println(path.toString());
 
 
 //        String name = given()
+//        .spec(reqSpec)
 //                .when()
-//                .get("http://restapi.wcaquino.me/usersXML")
+//                .get("usersXML")
 //                .then()
-//                .statusCode(200)
+//                .spec(respSpec)
 //                .extract().path("users.user.name.findAll{it.toString().startsWith('Maria')}")
 //
 //                ;
 //        Assert.assertEquals("Maria Joaquina".toUpperCase(), name.toUpperCase());
 
         ArrayList<Node> nomes = given()
+                .spec(reqSpec)
                 .when()
-                .get("http://restapi.wcaquino.me/usersXML")
+                .get("usersXML")
                 .then()
-                .statusCode(200)
+                .spec(respSpec)
                 .extract().path("users.user.name.findAll{it.toString().contains('n')}")
 
                 ;
--- src/main/java/XpathValidations.java
+++ src/main/java/XpathValidations.java
@@ -1,3 +1,10 @@
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -7,13 +14,32 @@ import static org.hamcrest.Matchers.*;
 
 public class XpathValidations {
 
+    public static RequestSpecification reqSpec;
+    public static ResponseSpecification respSpec;
+
+    @BeforeClass
+    public static void setUp(){
+        RestAssured.baseURI = "http://restapi.wcaquino.me";
+
+        RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+        reqBuilder.log(LogDetail.ALL);
+        reqSpec = reqBuilder.build();
+
+        ResponseSpecBuilder respBuilder = new ResponseSpecBuilder();
+        respBuilder.expectStatusCode(200);
+        respSpec = respBuilder.build();
+
+        RestAssured.requestSpecification = reqSpec;
+        RestAssured.responseSpecification = respSpec;
+    }
+
     @Test
     public void validacoesUrtilizandoXpath(){
         given()
                 .when()
                 .get("http://restapi.wcaquino.me/usersXML")
                 .then()
-                .statusCode(200)
+//                .statusCode(200)
                 .body(hasXPath("count(/users/user)", is("3")))
                 .body(hasXPath("/users/user[@id = '1']"))
                 .body(hasXPath("//user[age < 24]/name", is("Ana Julia")))
