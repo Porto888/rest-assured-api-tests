bf42421ba06e77251608aa4997bf287144b3c8c4
Porto888 <guilhermepstk@hotmail.com>
2024-01-02 08:40:26 -0300

    Continuação das validações tipo PostJson

--- src/main/java/jsonTests/PostJsonValidations.java
+++ src/main/java/jsonTests/PostJsonValidations.java
@@ -12,8 +12,7 @@ import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.config.EncoderConfig.encoderConfig;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 public class PostJsonValidations {
 
@@ -24,33 +23,52 @@ public class PostJsonValidations {
     public static void setUp(){
         RestAssured.baseURI = "https://restapi.wcaquino.me";
 
-        RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
-        reqBuilder.log(LogDetail.ALL);
-        reqSpec = reqBuilder.build();
-
-        ResponseSpecBuilder respBuilder = new ResponseSpecBuilder();
-        respBuilder.expectStatusCode(201);
-        respSpec = respBuilder.build();
-
-        RestAssured.requestSpecification = reqSpec;
-        RestAssured.responseSpecification = respSpec;
+//        RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+//        reqBuilder.log(LogDetail.ALL);
+//        reqSpec = reqBuilder.build();
+//
+//        ResponseSpecBuilder respBuilder = new ResponseSpecBuilder();
+//        respBuilder.expectStatusCode(201);
+//        respSpec = respBuilder.build();
+//
+//        RestAssured.requestSpecification = reqSpec;
+//        RestAssured.responseSpecification = respSpec;
     }
     @Test
     public void salvarUsuario (){
         given()
+                .log().all()
                 .contentType("application/json")
                 .body("{ \"name\": \"Porto\", \"age\": 31}")
                 .when()
                 .post("/users")
                 .then()
+                .log().all()
+                .statusCode(201)
                 .body("id", is(notNullValue()))
                 .body("name", is("Porto"))
-
-
+//                .body("age", is("31"))
 
         ;
 
+    }
 
+    @Test
+    public void naoSalvarUsuarioSemNome(){
+        given()
+                .log().all()
+                .contentType("application/json")
+                .body("{ \"age\": 31}")
+                .when()
+                .post("/users")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .body("id", is(nullValue()))
+                .body("error", is("Name é um atributo obrigatório"))
+
+
+        ;
     }
 
 
